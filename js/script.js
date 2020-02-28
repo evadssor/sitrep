911f54988d77b34efe753c790a9f630a369e883b
Dave <evadssor@gmail.com>
2020-02-27 23:31:55 -0600

    Lots o'changes

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let addStore = document.querySelector('#addStore');
+let printRep = document.querySelector('#printRep');
+
+addStore.addEventListener('click', function(){
+    alert('New Store Added');
+})
+
+printRep.addEventListener('click', function(){
+    alert('Printing Report');
+})
