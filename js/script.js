709ffde7d4e802fec6ecea999b0aa345c35ea32b
Dave Ross <evadssor@gmail.com>
2020-02-28 17:58:44 -0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-let addStore = document.querySelector('#addStore');
-let printRep = document.querySelector('#printRep');
+let addStore = document.querySelector('.addStore');
+let printRep = document.querySelector('.printRep');
 
 addStore.addEventListener('click', function(){
     alert('New Store Added');
 })
 
 printRep.addEventListener('click', function(){
-    alert('Printing Report');
+    alert('Printing Report...');
 })
