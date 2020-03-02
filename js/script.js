a19c74aa3b81ce57d0154f7d5ac23159c7a445e6
Dave Ross <evadssor@gmail.com>
2020-03-01 21:42:55 -0600

    redesign

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
 let addStore = document.querySelector('.addStore');
 let printRep = document.querySelector('.printRep');
+let post_up = document.querySelector('.post_up');
+let delete_up = document.querySelector('.delete_up');
+let update_btn = document.querySelector('.update_btn');
+let resolve = document.querySelector('.resolve');
+let add_update = document.querySelector('.add');
 
 addStore.addEventListener('click', function(){
     alert('New Store Added');
 })
 
 printRep.addEventListener('click', function(){
-    alert('Printing Report...');
-})
+    alert("Printing Today's Report...");
+});
+
+post_up.addEventListener('click', function(){
+    alert('Update Added Successfully!');
+});
+
+delete_up.addEventListener('click', function(){
+    confirm('Are you sure you want to cancel this update?');
+    if(confirm){
+        
+    }
+});
